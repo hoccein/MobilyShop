534bebf6f5f0b5375e7307d054189839d864708b
hosein kordloo <hosein.kordloo@gmail.com>
2018-09-04 12:05:01 +0430

    created MorePhonesActivity

---
+++ app/src/main/java/com/ynwa/kdl/hosein/shopping/activity_without_mvp/MorePhonesActivity.java
@@ -0,0 +1,15 @@
+package com.ynwa.kdl.hosein.shopping.activity_without_mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ynwa.kdl.hosein.shopping.R;
+
+public class MorePhonesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_phones);
+    }
+}
